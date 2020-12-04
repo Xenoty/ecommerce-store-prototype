bd395dae7f1af005a36fc25880441c9dde3a60c7
Xenoty <jacqueso.olivier@gmail.com>
2020-12-04 17:35:36 +0200

    All ViewModels requried for shop added

---
+++ SurfLocalShop/ViewModels/BasketViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SurfLocalShop.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurfLocalShop.ViewModels
+{
+    public class BasketViewModel
+    {
+        public List<BasketLine> BasketLines { get; set; }
+        [Display(Name = "Basket Total:")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal TotalCost { get; set; }
+    }
+}
