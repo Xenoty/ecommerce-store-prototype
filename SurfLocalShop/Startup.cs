0c977638f6b59b4dc03264021f3d8091e0b6b661
Xenoty <jacqueso.olivier@gmail.com>
2020-12-04 17:20:32 +0200

    Add project files.

---
+++ SurfLocalShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurfLocalShop.Startup))]
+namespace SurfLocalShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
