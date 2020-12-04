bde4b9da9961649d3b8af1f5761df6e6cc589d66
Xenoty <jacqueso.olivier@gmail.com>
2020-12-04 17:28:10 +0200

    Added all models required for e-commerce website

---
+++ SurfLocalShop/Models/ProductMetaData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurfLocalShop.Models
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+
+    public class ProductMetaData
+    {
+        [Display(Name = "Product Name")]
+        public string Name;
+    }
+}
