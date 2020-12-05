525c012ddd12752b9cf9463df09cd474e2f6a723
Xenoty <jacqueso.olivier@gmail.com>
2020-12-05 09:45:09 +0200

    added custom configuration for migrations

---
+++ SurfLocalShop/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Migrations;
+
+namespace SurfLocalShop.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<SurfLocalShop.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SurfLocalShop.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(SurfLocalShop.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
